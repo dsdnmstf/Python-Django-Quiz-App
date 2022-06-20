985ea4022c4de2e7a11111ea740c4c7d3b5b3dbb
musrafa <dsdnmstf@gmail.com>
2022-06-20 10:29:27 +0200

    quiz model added

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -6,3 +7,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    class meta:
+        verbose_name_plural = "Categories"
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Quiz Title")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Quizzes"
+
+
