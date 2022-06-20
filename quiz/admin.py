cc05220838f71c2f4f4b19a2db61966d89c8bfd9
musrafa <dsdnmstf@gmail.com>
2022-06-20 11:45:35 +0200

    admin added

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,8 @@
+from atexit import register
 from django.contrib import admin
-
+from .models import Category, Quiz, Question, Answer
 # Register your models here.
+admin.site,register(Category)
+admin.site,register(Question)
+admin.site,register(Quiz)
+admin.site,register(Answer)
