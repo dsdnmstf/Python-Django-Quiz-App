3d1f64e3389c66d1bf8df72383bef54b8ff335b1
musrafa <dsdnmstf@gmail.com>
2022-06-20 12:53:19 +0200

    admin customized

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,8 +1,22 @@
 from atexit import register
 from django.contrib import admin
+import nested_admin
 from .models import Category, Quiz, Question, Answer
+
+class AnswerInLine(nested_admin.NestedTabularInline):
+    model = Answer
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInLine]
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionInline]
+
+
 # Register your models here.
-admin.site,register(Category)
-admin.site,register(Question)
-admin.site,register(Quiz)
-admin.site,register(Answer)
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Answer)
