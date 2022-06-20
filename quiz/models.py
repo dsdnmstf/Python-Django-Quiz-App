5261fc90a67bace8f0a232e13070ec347c63069f
musrafa <dsdnmstf@gmail.com>
2022-06-20 11:43:35 +0200

    .

--- quiz/models.py
+++ quiz/models.py
@@ -1,4 +1,4 @@
-from turtle import ondrag
+from turtle import ondrag, update
 from unicodedata import category
 from django.db import models
 
@@ -23,8 +23,35 @@ class Quiz(models.Model):
     class Meta:
         verbose_name_plural = "Quizzes"
 
-class Question(models.Model):
+class Update(models.Model):
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Question(Update):
+    SCALE = (
+        (0, "beginner"),
+        (1, "Intermediate"),
+        (2,"Advenced")
+    )
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     title = models.CharField(max_length=300, verbose_name="question")
+    difficulty = models.IntegerField(choices=SCALE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    # updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Answer(Update):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=500 )
+    is_right = models.BooleanField(default=False)
+    # updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.answer_text
+    
 
 
